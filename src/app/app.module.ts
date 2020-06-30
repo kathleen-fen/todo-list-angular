5536267802ffd96825e47794430d963edbddcd43
Kate Fenchenko <fev3@mail.ru>
2020-06-30 19:01:52 +0300

    libs for drag and drop installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TaskComponent } from './task/task.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 
 
@@ -15,7 +16,8 @@ import { TaskComponent } from './task/task.component';
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
